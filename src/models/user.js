947c4a8e23d4d7d7dd47490b36fdd99f6ecf0cc7
soroush <soroushfalaki24@hotmail.com>
2026-06-23 20:28:04 +0330

    add bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,23 @@
 import { sequelize } from '../config/db.js';
 import { DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 export const User = sequelize.define(
   'User',
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
       primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
       unique: true,
     },
+
     username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -23,6 +26,7 @@ export const User = sequelize.define(
         isEmail: true,
       },
     },
+
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -32,14 +36,29 @@ export const User = sequelize.define(
       type: DataTypes.ENUM('user', 'admin', 'instructor'),
       defaultValue: 'user',
     },
+
     isActive: {
       type: DataTypes.BOOLEAN,
       defaultValue: true,
     },
+
     refreshToken: {
       type: DataTypes.TEXT,
       allowNull: true,
     },
   },
-  { tableName: 'users', timestamps: true, underscored: true },
+  {
+    tableName: 'users',
+    timestamps: true,
+    underscored: true,
+
+    hooks: {
+      async beforeSave(user) {
+        if (!user.changed('password')) return;
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      },
+    },
+  },
 );
