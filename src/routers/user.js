facc94d38b61577f05a0d3224bec0ca9ee4ded13
soroush <soroushfalaki24@hotmail.com>
2026-06-28 14:08:09 +0330

    add current user router

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { getAllUsers } from '../controllers/user/getAllUsers.js';
 import { getUser } from '../controllers/user/getUser.js';
 import { updateUser } from '../controllers/user/updateUser.js';
+import { currentUser } from '../controllers/user/currentUser.js';
 
 import { globalLimiter } from '../utils/rateLimiter.js';
 import { authorizePermissions } from '../middlewares/authorizePermissions.js';
@@ -24,6 +25,14 @@ userRouter.get(
   authenticateUser,
   authorizePermissions('admin'),
   getUser,
+);
+
+userRouter.get(
+  '/:id',
+  globalLimiter,
+  authenticateUser,
+  authorizePermissions('admin', 'user'),
+  currentUser,
 );
 
 userRouter.patch(
