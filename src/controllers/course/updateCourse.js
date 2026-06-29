5355e767deb572321d4f70c21a3a6bb66272eb52
soroush <soroushfalaki24@hotmail.com>
2026-06-29 17:09:55 +0330

    some change

--- src/controllers/course/updateCourse.js
+++ src/controllers/course/updateCourse.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from '../../middlewares/asyncHandler.js';
 import { Course } from '../../models/course.js';
 import { BadRequestError } from '../../errors/badRequest.js';
+import { logger } from '../../utils/logger.js';
 
 export const updateCourse = asyncHandler(async (req, res) => {
   const course = await Course.findByPk(req.params.id);
@@ -8,6 +9,8 @@ export const updateCourse = asyncHandler(async (req, res) => {
   if (!course) throw new BadRequestError('course not found', 400);
 
   const updateCourse = await Course.update(req.body);
+
+  logger.info(`course with id:${req.params.id} updated`);
 
   return res.status(200).json({
     success: true,
