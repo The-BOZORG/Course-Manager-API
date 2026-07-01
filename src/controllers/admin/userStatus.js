d56c174b8cdd670e2cb0ed87ebfb57f7ee07d34a
soroush <soroushfalaki24@hotmail.com>
2026-07-01 20:37:07 +0330

    add status user controller

---
+++ src/controllers/admin/userStatus.js
@@ -0,0 +1,23 @@
+import { logger } from '../../utils/logger.js';
+import { asyncHandler } from '../../middlewares/asyncHandler.js';
+import { User } from '../../models/associations.js';
+import { NotFoundError } from '../../errors/notFound.js';
+
+export const changeRole = asyncHandler(async (req, res) => {
+  const { isActive } = req.body;
+  const user = await User.findByPk(req.params.id);
+
+  if (!user) throw new NotFoundError('user not found', 404);
+
+  user.isActive = isActive;
+
+  await user.save();
+
+  logger.info(`user with id:${req.params.id} changed success`);
+
+  return res.status(200).json({
+    success: true,
+    message: 'User status updated success',
+    data: user,
+  });
+});
