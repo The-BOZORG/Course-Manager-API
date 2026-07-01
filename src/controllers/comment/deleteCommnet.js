be62abbaeb38677102fbb92eaa464e1ece12e1b7
soroush <soroushfalaki24@hotmail.com>
2026-07-01 10:55:39 +0330

    ada delete commnet controller

---
+++ src/controllers/comment/deleteCommnet.js
@@ -0,0 +1,19 @@
+import { asyncHandler } from '../../middlewares/asyncHandler.js';
+import { Comment } from '../../models/comment.js';
+import { NotFoundError } from '../../errors/notFound.js';
+import { logger } from '../../utils/logger.js';
+
+export const deleteComment = asyncHandler(async (req, res) => {
+  const comment = await Comment.findByPk(req.params.id);
+
+  if (!comment) throw new NotFoundError('comment not found', 404);
+
+  await comment.destroy();
+
+  logger.info(`comment with this id: ${req.params.id} deleted`);
+
+  return res.status(200).json({
+    success: true,
+    message: 'delete comment success',
+  });
+});
--- src/routers/comment.js
+++ src/routers/comment.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { createComment } from '../controllers/comment/createCommnet.js';
 import { getComments } from '../controllers/comment/getCommnet.js';
+import { getCommentById } from '../controllers/comment/getCommentById.js';
 
 import { globalLimiter } from '../utils/rateLimiter.js';
 import { authenticateUser } from '../middlewares/authentication.js';
@@ -11,5 +12,12 @@ const commentRouter = Router();
 commentRouter.get('/getComments', globalLimiter, authenticateUser, getComments);
 
 commentRouter.post('/create', globalLimiter, authenticateUser, createComment);
+
+commentRouter.get(
+  '/getComment/:id',
+  globalLimiter,
+  authenticateUser,
+  getCommentById,
+);
 
 export default commentRouter;
