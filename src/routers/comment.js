09b2c1c4f87a46e2611cadc1ca4706e809097963
soroush <soroushfalaki24@hotmail.com>
2026-07-01 10:46:38 +0330

    add getcommnet router

--- src/routers/comment.js
+++ src/routers/comment.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { createComment } from '../controllers/comment/createCommnet.js';
+import { getComments } from '../controllers/comment/getCommnet.js';
 
 import { globalLimiter } from '../utils/rateLimiter.js';
 import { authenticateUser } from '../middlewares/authentication.js';
 
 const commentRouter = Router();
+
+commentRouter.get('/getComments', globalLimiter, authenticateUser, getComments);
 
 commentRouter.post('/create', globalLimiter, authenticateUser, createComment);
 
